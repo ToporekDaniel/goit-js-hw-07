96692af9b24bb2a412a865633967ae57b8406d7b
ToporekDaniel <danieltoporek0@gmail.com>
2023-11-20 20:33:58 +0100

    hmm

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  const pic = `<li>
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>`;
+
+  gallery.insertAdjacentHTML("beforeend", pic);
+}
+
+const galleryItem = document.querySelectorAll(".gallery__item");
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const imgLink = event.target.closest(".gallery__image");
+  if (!imgLink) return;
+
+  const bigImgSrc = imgLink.getAttribute("data-source");
+
+  const lightbox = basicLightbox.create(`
+    <img src="${bigImgSrc}" alt="${event.target.alt}" />`);
+  lightbox.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      lightbox.close();
+    }
+  });
+});
